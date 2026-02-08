4af7a4ae8da6da872a3f1865ab888e79cfd80e5c
Youhei Yamada <51235130+sun-flat-yamada@users.noreply.github.com>
2026-02-09 08:08:26 +0900

    fix: Electron-appでFILESペインのファイルが開けない不具合を修正

--- packages/electron-app/src/renderer/views/RecentFilesView.ts
+++ packages/electron-app/src/renderer/views/RecentFilesView.ts
@@ -9,14 +9,22 @@ export class RecentFilesView extends Component {
 
   public render(files: string[]): void {
     this.element.innerHTML = "";
-    files.forEach((file) => {
+    // Remove duplicates to prevent ID collisions and redundant entries
+    const uniqueFiles = [...new Set(files)];
+
+    uniqueFiles.forEach((file, index) => {
       const item = document.createElement("div");
       item.className = "recent-file-item";
+      item.id = `recent-file-${index}`;
       item.innerText = file.split(/[\\/]/).pop() || "";
       item.title = file;
-      item.onclick = () => {
-        if (this.onFileClick) this.onFileClick(file);
-      };
+
+      item.addEventListener("click", () => {
+        if (this.onFileClick) {
+          this.onFileClick(file);
+        }
+      });
+
       this.element.appendChild(item);
     });
   }
